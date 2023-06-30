bbb1bf3e930a8c31afcadceb40ad811267fb883f
kenbadertscher <98230845+kenbadertscher@users.noreply.github.com>
2023-06-30 11:03:20 +0000

    remote test

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,8 +1,29 @@
 'use client';
 
+import { motion } from 'framer-motion';
+
+import styles from '../styles';
+import { staggerContainer, fadeIn } from '../utils/motion';
+import { TitleText, TypingText } from '../components';
+
 const World = () => (
-  <section>
-    World section
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: 'false', amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="| People around the World"
+        textStyles="text-center" />
+      <TitleText
+        title={<>
+          Track friends around you and invite them to play together in the same world
+        </>}
+        textStyles="text-center"
+      />
+    </motion.div>
   </section>
 );
 
