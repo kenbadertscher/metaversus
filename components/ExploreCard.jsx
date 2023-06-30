708913f87d7aa263d232ba7f965b7f2c4b9a3c92
kenbadertscher <badertscher.ken@gmail.com>
2023-06-29 20:33:07 -0400

    even more linting

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -14,9 +14,9 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
     onClick={() => handleClick(id)}
   >
     <img
-        src={imgUrl}
-        alt={title}
-        className="absolute w-full h-full object-cover rounded-[24px]"
+      src={imgUrl}
+      alt={title}
+      className="absolute w-full h-full object-cover rounded-[24px]"
     />
     {active !== id ? (
       <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]">
